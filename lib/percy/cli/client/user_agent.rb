8386222f6d4fa9ec26f843a0ffbe1f0f718a9ae2
David Jones <dgjones@gmail.com>
2017-06-26 11:56:24 -0700

    fix linting and generally apply rubocop config from our other projects for consistency

--- lib/percy/cli/client/user_agent.rb
+++ lib/percy/cli/client/user_agent.rb
@@ -11,7 +11,7 @@ module Percy
             "middleman/#{_middleman_version}",
             "jekyll/#{_jekyll_version}",
           ].reject do |info|
-            info =~ %r{\/$} # reject if version is empty
+            info =~ /\/$/ # reject if version is empty
           end.join('; ')
         end
 
@@ -19,14 +19,14 @@ module Percy
           begin
             require 'middleman-core/version'
             Middleman::VERSION if defined? Middleman
-          rescue LoadError; end
+          rescue LoadError; end # rubocop:disable HandleExceptions
         end
 
         def _jekyll_version
           begin
             require 'jekyll/version'
             Jekyll::VERSION if defined? Jekyll
-          rescue LoadError; end
+          rescue LoadError; end # rubocop:disable HandleExceptions
         end
       end
     end
